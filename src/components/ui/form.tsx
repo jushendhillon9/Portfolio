fd8631ddf82892e95f2977450cb83981a11b20bf
“Jushen <jsdhillo@usc.edu>
2025-09-08 21:15:49 -0700

    fixing vercel deployment

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,18 +1,18 @@
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
 import * as React from "react";
-import {
-  Controller,
+import { Controller, FormProvider, useFormContext, useFormState } from "react-hook-form";
+
+// type-only imports
+import type {
   ControllerProps,
   FieldPath,
   FieldValues,
-  FormProvider,
-  useFormContext,
-  useFormState,
+  UseControllerProps,
 } from "react-hook-form";
 
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
+import { Label } from "../ui/label";
+import { cn } from "../ui/utils";
 
 const Form = FormProvider;
 
