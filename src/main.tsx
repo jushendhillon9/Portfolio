19542660b54bd98056e2ebab5dc2f8a45241de19
“Jushen <jsdhillo@usc.edu>
2025-09-09 21:33:13 -0700

    fixing troubleshooting

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import App from "./App.tsx";
 
 const el = document.getElementById("root");
 if (!el) throw new Error("Root element #root not found");
